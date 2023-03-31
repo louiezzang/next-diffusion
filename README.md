8678c7509a9322bff60f34e7c57a0e2142221adb
Younggue Bae <ybae2@lululemon.com>
2023-03-30 18:44:24 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # nextmining-diffusion
+---
+
+
+# Requirements
+python >= 3.8
+
+# Development Environment
+This is for setting up your IDE development environment.
+```
+$ pipenv install --skip-lock -r requirements.txt 
+```
+To activate this project's virtualenv
+```
+$ pipenv shell
+```
+
+# Build Package
+```
+$ python3 setup.py bdist_wheel
+```
+
+# References
+- Huggingface Diffusers 
+    https://github.com/huggingface/diffusers/tree/main/examples/
+- Jeremy Howard’s Course of From Deep Learning Foundations to Stable Diffusion
+    https://www.fast.ai/posts/part2-2022-preview.html  
+    Lesson 9: Deep Learning Foundations to Stable Diffusion, 2022 (https://youtu.be/_7rMfsA24Ls)
+    https://github.com/fastai/diffusion-nbs
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+"""Builds the package.
+@author: Younggue Bae
+"""
+from __future__ import print_function
+import os
+import sys
+
+import pkg_resources
+from setuptools import setup, find_packages
+
+
+if sys.version_info < (2, 7):
+    print("Python versions prior to 2.7 are not supported.",
+          file=sys.stderr)
+    exit(-1)
+
+setup_requires = [
+]
+
+dependency_links = [
+    # "https://download.pytorch.org/whl/nightly/cu116" # For PyTorch2.0
+]
+
+setup(
+    name="next-diffusion",
+    py_modules=["next-diffusion"],
+    version="1.0.0",
+    description="nextDiffusion",
+    author="Younggue Bae",
+    author_email="",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    # install_requires=install_requires,
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
+    include_package_data=True,
+    # extras_require={'dev': ['pytest']},
+    setup_requires=setup_requires,
+    dependency_links=dependency_links,
+)
