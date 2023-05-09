83c4a4e68dae234aa5fb4e21b9af4531aede1608
Younggue Bae <ybae2@lululemon.com>
2023-05-08 23:48:24 -0700

    expose parse_args function

--- src/nextdiffusion/textual_inversion/__init__.py
+++ src/nextdiffusion/textual_inversion/__init__.py
@@ -1,7 +1,8 @@
 from .textual_inversion_dataset import TextualInversionDataset
-from .textual_inversion_trainer import TextualInversionTrainer
+from .textual_inversion_trainer import TextualInversionTrainer, parse_args
 
 __all__ = [
     'TextualInversionDataset',
     'TextualInversionTrainer',
+    'parse_args'
 ]
